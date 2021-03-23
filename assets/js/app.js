c554b199b88f90b1081a2811547c8373e7d40ad8
Maksym <maksymRozhko7@gmail.com>
2021-03-23 22:36:30 +0200

    feat(modal-cart);
    - add files;
    - add open/close modal-cart;
    - add smoth scroll;

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,4 +4,37 @@ const swiper = new Swiper('.swiper-container', {
       nextEl: '.slider-button-next',
       prevEl: '.slider-button-prev',
     },
-  });
+});
+
+// cart 
+
+const buttonCart = document.querySelector('.button-cart');
+const modalCart = document.querySelector('#modal-cart');
+const buttonModalClose = document.querySelector('.modal-close');
+
+const openModalCart = () => {
+  modalCart.classList.add('show');
+};
+
+const closeModalCart = () => {
+  modalCart.classList.remove('show');
+};
+
+buttonCart.addEventListener('click', openModalCart);
+buttonModalClose.addEventListener('click', closeModalCart);
+
+// scroll smooth
+
+{
+  const scrollLinks = document.querySelectorAll('a.scroll-link');
+  for (let i = 0; i < scrollLinks.length; i++) {
+    scrollLinks[i].addEventListener('click', e => {
+      e.preventDefault();
+      const linkId = scrollLinks[i].getAttribute('href');
+      document.querySelector(linkId).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    });
+  };
+}
